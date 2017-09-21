8fedfa614d9c66cd91e5c06026eadff2abdb8253
ameweber <32169950+ameweber@users.noreply.github.com>
2017-09-21 17:56:27 +0300

    Update riddles.py

--- homework1/riddles.py
+++ homework1/riddles.py
@@ -1,11 +1,1 @@
-print('Введите первое число: ')
-a = float(input())
-print('Введите второе число: ')
-b = float(input())
-print('"+" или "-" ? ')
-symbol = raw_input()
-if symbol == '-':
-    s = a - b
-elif symbol == '+':
-    s = a + b
-print(s)
+
